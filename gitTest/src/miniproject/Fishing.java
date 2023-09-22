8c7206480aa1dedd2f882c403bf59d579a6992c3
naraeha <dkfmrhs170@gmail.com>
2023-09-22 09:14:12 +0900

    2023 9 22 김다원 주석 추가

--- gitTest/src/miniproject/Fishing.java
+++ gitTest/src/miniproject/Fishing.java
@@ -7,6 +7,7 @@ public class Fishing {
 	private double max; 
 	private Scanner sc = new Scanner(System.in);
 	
+	//미끼 선택 (변경 가능성 높음)
 	public int getBait(){
 		System.out.println("미끼를 선택하세요");
 		int bait = sc.nextInt();
@@ -22,12 +23,15 @@ public class Fishing {
 		
 	}
 	 
-
+	//낚시 시작후 낚싯대 잡아채는 과정
 	public void fishing() {
 		int baittype = getBait();
-		int baitcnt = 50;
+		int baitcnt = 50;//남은 미끼 수
 		while (true) {
 			System.out.println("남은 미끼 수 : "+baitcnt);
+			
+			//타이머
+			// timer 값에 따라 정해진 시간만큼 지연된 후에 다음 코드가 출력됨
 			int timer = rd.nextInt(5)+5;
 			long now = System.currentTimeMillis();	
 			long currentTime = 0;
@@ -37,6 +41,11 @@ public class Fishing {
 			while (currentTime - now < timer*1000) {
 				currentTime = System.currentTimeMillis();
 			}
+			
+			//타이머 끝
+			
+			//물고기 크기에 따라 느낌표 수가 다르게 표시했음
+			//물고기 낚이는 크기는 db와 연동할 예정
 			int fishSize = rd.nextInt(3)+1;
 			if(fishSize == 1) {
 				System.out.println("!");
@@ -45,12 +54,17 @@ public class Fishing {
 			}else if(fishSize == 3) {
 				System.out.println("!!!");
 			}
+			
+			//물고기 잡아채는 과정
 			System.out.println("키를 연타해서 물고기를 잡으세요!");
 			now = System.currentTimeMillis();
 			int cnt = 0;
+			
+			//while문 안의 조건(5초가 넘어가면 반복문이 끝남)
 			while(currentTime - now < 5000) {
 				currentTime = System.currentTimeMillis();
 				
+				//입력값이 스페이스바나 null이 아니면 cnt 값이 올라감 
 				String getFish = sc.next();
 				if(getFish != null) {
 					cnt++;
@@ -58,15 +72,18 @@ public class Fishing {
 				for(int i = 0; i<cnt; i++) {
 					System.out.print("■");
 				}
+				//cnt값이 올라서 최대치까지 올라가기 전에 메세지 출력
 				if(cnt >7) {
 					System.out.println("거의 다 잡았어요!");
 				}
+				//10이면 물고기 낚시 성공
 				if(cnt == 10) {
 					break;
 				}
 			}
 			System.out.println();
 			if(cnt >= 10) {
+				//낚시 성공시 출력될 문장
 				System.out.println("낚싯대를 잡아 올렸습니다!");
 				System.out.println("낚싯대를 잡아 올렸습니다!");
 				System.out.println("낚싯대를 잡아 올렸습니다!");
@@ -75,8 +92,10 @@ public class Fishing {
 				baitcnt--;
 				fightFish();
 			}else {
+				//낚시 실패시 출력될 문장
 				System.out.println("놓쳤습니다...");
 				baitcnt--;
+				//다음 낚시까지 잠시 지연됨
 				now = System.currentTimeMillis();	
 				currentTime = 0;
 				while (currentTime - now < 5000) {
@@ -88,12 +107,15 @@ public class Fishing {
 		}
 	}
 	
+	//낚싯대를 잡아채는 방법과 낚인 물고기 레벨에 따라 낚시 성공 실패를 가를 예정
 	public void fightFish() {
 		long now = System.currentTimeMillis();	
 		long currentTime = 0;
 		while (currentTime - now < 3000) {
 			currentTime = System.currentTimeMillis();
 		}
+		//입질때 느낌표 개수에 따라서 fishsize가 변경됨
+		//작은 물고기는 섬세한 낚아채기, 큰 물고기는 강력한 낚아채기가 필요
 		int fishsize = 1;
 		System.out.println("물고기를 낚아챌 방법을 고르세요.");
 		System.out.println("1. 섬세한 낚아채기 2. 강력한 낚아채기");
@@ -102,14 +124,14 @@ public class Fishing {
 			System.out.println("물고기를 낚았습니다!");
 			resultFish();	
 		}else{
-			System.out.println("놓쳤습니다....");
+			System.out.println("물고기가 도망갔습니다...");
 			
 		}
 		
 		
 		
 	}
-	
+	//낚시 결과물
 	public void resultFish(){
 		String getFishname = "";
 		double getFishsize = 0;
