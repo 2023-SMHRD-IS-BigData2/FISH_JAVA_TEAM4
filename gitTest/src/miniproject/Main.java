fc64990bf987f9933f9821b0c62f5d4a11070f12
user <user@121.147.0.204>
2023-09-22 16:42:32 +0900

    회원가입 로그인 구현

--- gitTest/src/miniproject/Main.java
+++ gitTest/src/miniproject/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
 		
-		
-
+		DAO mdao = new DAO();
+		mdao.insertUser();
+		mdao.userLogin();
 		Controller fs = new Controller();
 		int stage = fs.getStage();
 		fs.fishing(stage);
