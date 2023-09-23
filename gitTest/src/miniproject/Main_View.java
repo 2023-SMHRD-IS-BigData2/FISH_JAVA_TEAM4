3861708a9b75c5df019ead76f71bcdea16d78418
naraeha <dkfmrhs170@gmail.com>
2023-09-23 13:20:26 +0900

    1 20

--- gitTest/src/miniproject/Main_View.java
+++ gitTest/src/miniproject/Main_View.java
@@ -1,10 +1,16 @@
 package miniproject;
+
+import java.util.Scanner;
+
 public class Main_View {
 
 	public static void main(String[] args) {
-
-		
 		DAO mdao = new DAO();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+		System.out.println("1. 회원가입 2. 로그인&게임시작 3. 랭킹확인");
+		
 		//1. 회원가입
 		mdao.insertUser();
 		//2. 로그인
@@ -15,7 +21,7 @@ public class Main_View {
 		fs.finalResult(user);
 		//3. 랭킹확인
 		fs.printrank();
-		 
+		}
 		  
 
 	}
